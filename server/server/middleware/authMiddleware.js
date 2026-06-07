220b073cb7942908bbbb3ee12343434e33411716
Ramkumar1801 <ramkumarcivil7@gmail.com>
2026-06-07 10:40:43 +0530

    Update authMiddleware.js

--- server/server/middleware/authMiddleware.js
+++ server/server/middleware/authMiddleware.js
@@ -1,1 +1,34 @@
+const jwt = require("jsonwebtoken");
 
+module.exports = (req, res, next) => {
+
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "No Token, Access Denied"
+    });
+  }
+
+  try {
+
+    const decoded = jwt.verify(
+      token.replace("Bearer ", ""),
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+
+    next();
+
+  } catch (err) {
+
+    return res.status(401).json({
+      success: false,
+      message: "Invalid Token"
+    });
+
+  }
+
+};
