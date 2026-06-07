16e9e4b7de8b75b2b7c664fe03538d612c046a74
Ramkumar1801 <ramkumarcivil7@gmail.com>
2026-06-07 10:00:14 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/login", (req, res) => {
+  res.json({
+    success: true,
+    message: "Login Working"
+  });
+});
+
 router.post("/login", (req, res) => {
   res.json({
     success: true,
