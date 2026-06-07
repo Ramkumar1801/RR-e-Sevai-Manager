d7b94ba965ca75546103f705d480a4f38926b2fc
Ramkumar1801 <ramkumarcivil7@gmail.com>
2026-06-07 07:53:19 +0530

    Create server.js

---
+++ server/server/middleware/server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB =
+require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  "/api/auth",
+  require("./routes/auth")
+);
+
+app.get("/", (req, res) => {
+
+  res.json({
+    app: "RR e-Sevai Manager",
+    status: "Running"
+  });
+
+});
+
+const PORT =
+process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${PORT}`
+  );
+});
